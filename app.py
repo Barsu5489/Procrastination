0608464a27a01d24d4011449e3e069468cd34621
barsu5489 <ebarsulai@gmail.com>
2023-06-28 22:52:01 +0300

    basic routes in flask

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World"
+@app.route('/about')
+def about():
+    return "About page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
