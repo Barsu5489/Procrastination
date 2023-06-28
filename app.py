ee47d69fe1183aa80b1a8e0aabf5d57c413b6e40
barsu5489 <ebarsulai@gmail.com>
2023-06-28 23:13:50 +0300

    template

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
-@app.route('/about')
-def about():
-    return "About page"
+    return render_template('base.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
